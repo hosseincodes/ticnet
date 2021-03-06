64ed044cf8dbbff5f6d351f8020c6193b01906aa
Hossein Akbari <hosseinakbari506@gmail.com>
2021-03-06 13:25:06 +0330

    Minor changes

--- frontend/src/pages/login/LoginPage.js
+++ frontend/src/pages/login/LoginPage.js
@@ -1,26 +1,18 @@
 import React from 'react';
-// import Grid from "@material-ui/core/div";
-// import Typography from "@material-ui/core/Typography";
-// import TextField from "@material-ui/core/TextField";
-// import FormControl from "@material-ui/core/FormControl";
-// import div from "@material-ui/core/div";
-// import div from "@material-ui/core/div";
-// import Button from "@material-ui/core/Button";
-import './style.css'
-// import classNames from 'classnames';
-import axios from 'axios'
+import axios from 'axios';
+import './style.css';
 
 const LoginPage = (props) => {
 
   const [name, setName] = React.useState("");
 
-  //methods
   const validation = (user) => {
     console.log("validation", user);
     if (!user.name)
       return "You must enter a name"
     return null;
   }
+
   const submit = () => {
     console.log(name);
     const error = validation({name});
@@ -42,24 +34,36 @@ const LoginPage = (props) => {
   }
 
   return (
-    <div className="login-page-container">
-      <div className="login-box">
-      <div className="header">
-        <h1>Ticnet</h1>
+      <div className="login-page-container">
+
+        <div className="login-box">
+
+        <div className="header">
+
+          <h1>Ticnet</h1>
+
+        </div>
+
+        <div className="name-input">
+
+          <input
+            id="outlined-basic"
+            placeholder="Enter your name"
+            value={name}
+            onChange={e => setName(e.target.value)}
+          />
+
+        </div>
+
+        <div className="login-button">
+
+          <button variant="contained" color="primary" onClick={submit}>Login to the chat room</button>
+
+        </div>
+
+        </div>
+
       </div>
-      <div className="name-input">
-        <input
-          id="outlined-basic"
-          placeholder="Enter your name"
-          value={name}
-          onChange={e => setName(e.target.value)}
-        />
-      </div>
-      <div className="login-button">
-        <button variant="contained" color="primary" onClick={submit}>Login to the chat room</button>
-      </div>
-      </div>
-    </div>
   );
 };
 
