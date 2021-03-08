5c9fad9a82fdc0d5f077b85e78a20a84171b39bb
Hossein Akbari <hosseinakbari506@gmail.com>
2021-03-08 16:43:24 +0330

    Get the first [npm run build] from frontend (react)

---
+++ frontend/build/precache-manifest.6150d6f3bedd8583859779c910ad1014.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca783c92adc5f32190d724665070cc97",
+    "url": "/index.html"
+  },
+  {
+    "revision": "037c7897a471afcf4f3b",
+    "url": "/static/css/main.5c16a851.chunk.css"
+  },
+  {
+    "revision": "6fa6bf19ece2542654ac",
+    "url": "/static/js/2.34f6aeb2.chunk.js"
+  },
+  {
+    "revision": "df958f16b07871c3679510165e97a61a",
+    "url": "/static/js/2.34f6aeb2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "037c7897a471afcf4f3b",
+    "url": "/static/js/main.a3621ecc.chunk.js"
+  },
+  {
+    "revision": "c52060137d2dc1c720d0",
+    "url": "/static/js/runtime-main.d6e69434.js"
+  }
+]);
